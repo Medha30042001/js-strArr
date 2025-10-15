2717a0ef284b6ddbfc388da4f62a677560d43ed7
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-15 18:46:35 +0530

    Create 03-maximumInArray.js

---
+++ 03-maximumInArray.js
@@ -0,0 +1,20 @@
+//my attempt
+
+function maximumInArray(N,arr){
+  let max = arr[0];
+    for(let i=1; i<N; i++){
+      if(arr[i]>max){
+        max=arr[i];
+      }
+    }
+    console.log(max);
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  let b = a[1].split(' ').map(Number);
+  maximumInArray(parseInt(a[0], 10), b);
+}
+
+main();
