fd7563d6cb18a122c13de77285102770c9072c8b
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-15 18:43:22 +0530

    Create 01-printArray.js

---
+++ 01-printArray.js
@@ -0,0 +1,18 @@
+//my approach
+
+function printArray(N,arr){
+  
+    for(let i=0; i<N; i++){
+      console.log(arr[i]);
+    }
+    
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  let b = a[1].split(' ').map(Number);
+  printArray(parseInt(a[0], 10), b);
+}
+
+main();
