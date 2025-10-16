cce6a88fe5ca90170bb5c9df27c2a2465752db64
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:06:01 +0530

    Create 04-evenArray.js

---
+++ 04-evenArray.js
@@ -0,0 +1,19 @@
+//my attempt
+
+function evenArray(n, arr){
+    for(let i=0; i<n; i++){
+      if(arr[i]%2==0){
+        console.log(arr[i]);
+      }
+    }
+    
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  let b = a[1].split(' ').map(Number);
+  evenArray(parseInt(a[0], 10), b);
+}
+
+main();
