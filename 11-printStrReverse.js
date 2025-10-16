306bfc4ef1e1e4bccc50bb190069fa89b6355af7
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:20:55 +0530

    Create 11-printStrReverse.js

---
+++ 11-printStrReverse.js
@@ -0,0 +1,15 @@
+//my attempt
+
+function printCharReverse(N,str) {
+    for(let i=N-1; i>=0; i--){
+      console.log(str[i]);
+    }
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  printCharReverse(parseInt(a[0], 10), a[1]);
+}
+
+main();
