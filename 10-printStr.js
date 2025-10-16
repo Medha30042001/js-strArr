c7230b87301957a93593ac88df2395d034d28788
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:19:17 +0530

    Create 10-printStr.js

---
+++ 10-printStr.js
@@ -0,0 +1,17 @@
+//my attempt
+
+function printChar(N,str) {
+    for(let i=0; i<N; i++){
+      console.log(str[i]);
+    }
+    
+
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  printChar(parseInt(a[0], 10), a[1]);
+}
+
+main();
