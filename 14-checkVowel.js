daa7728ea250eccd79c290d1edb5860188261811
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:26:11 +0530

    Create 14-checkVowel.js

---
+++ 14-checkVowel.js
@@ -0,0 +1,23 @@
+//my attempt
+
+function checkVowel(N, str) {
+  let vowels = 'aeiouAEIOU';
+  let check = false;
+  
+  for(let ch of str){
+    if(vowels.includes(ch)){
+      check = true;
+      break;
+    }
+  }
+  console.log(check);
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  checkVowel(parseInt(a[0], 10), a[1]);
+}
+
+main();
+
