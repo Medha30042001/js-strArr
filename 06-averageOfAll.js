622d2c97bab5e869cd7da258d778c2a6ef756a41
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-16 16:09:12 +0530

    Create 06-averageOfAll.js

---
+++ 06-averageOfAll.js
@@ -0,0 +1,18 @@
+//my attempt
+
+function averageOfAll(n, arr){
+  let sum = 0;
+    for(let i=0; i<n; i++){
+      sum += arr[i];
+    }
+    console.log(Math.ceil(sum / n));
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim();
+  let a = input.split('\n');
+  let b = a[1].split(' ').map(Number);
+  averageOfAll(parseInt(a[0], 10), b);
+}
+
+main();
